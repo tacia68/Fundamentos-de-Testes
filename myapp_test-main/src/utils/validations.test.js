a92ca0042e1493b857e983e55b93f03d6f587e88
tacia68 <tacia68@gmail.com>
2023-07-25 20:57:51 -0400

    verifyStockAvailability()

--- myapp_test-main/src/utils/validations.test.js
+++ myapp_test-main/src/utils/validations.test.js
@@ -1,4 +1,4 @@
-const {firstName} = require("../utils/validations")
+const {firstName, verifyStockAvailability} = require("../utils/validations")
 
 describe("firtName()", () =>{
     it("should return the first name when the full name is given", () =>{
@@ -42,6 +42,39 @@ describe("firtName()", () =>{
         const result = firstName(fullName);
         expect(result).toBe("");
     });
+
+
+    //new test
+
+describe("verifyStockAvailability()", () => {
+    it("should return true when product type is available in stock", () => {
+        const productType = "laptop";
+        const qty = 5;
+        const result = verifyStockAvailability(productType, qty);
+        expect(result).toBe(true);
+    });
+
+    it("should return false when product type is sold out", () => {
+        const productType = "book";
+        const qty = 3;
+        const result = verifyStockAvailability(productType, qty);
+        expect(result).toBe(false);
+    });
+
+    it("should return false when product type has less stock qty than the requested", () => {
+        const productType = "tablet";
+        const qty = 20;
+        const result = verifyStockAvailability(productType, qty);
+        expect(result).toBe(false);
+    });
+
+    it("should return false when product type doesnt exists in stock", () => {
+        const productType = "other";
+        const qty = 3;
+        const result = verifyStockAvailability(productType, qty);
+        expect(result).toBe(false);
+    });
+});
 
  
    
